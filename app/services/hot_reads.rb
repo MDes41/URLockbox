2512d5233bd0671d2d614728bd0a2f80cf526137
Matt D <MDes41@users.noreply.github.com>
2017-03-01 11:53:29 -0700

    updated url

--- app/services/hot_reads.rb
+++ app/services/hot_reads.rb
@@ -5,10 +5,10 @@ class HotReads
   end
 
   def send_link_to_hot_reads(link)
-    conn = Faraday.new(:url => 'https://nameless-coast-56698.herokuapp.com/reads')
-    
+    conn = Faraday.new(:url => 'https://nameless-coast-56698.herokuapp.com')
 
     conn.post do |req|
+      req.url '/reads'
       req.params['link'] = link.to_json
     end
   end
